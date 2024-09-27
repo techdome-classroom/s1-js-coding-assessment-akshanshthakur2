ac6516c77a918220723e3703956738e4bf41d3f7
akshanshthakur2 <contact.akshans0@gmail.com>
2024-09-27 12:52:57 +0530

    completed testcase

--- program2.js
+++ program2.js
@@ -2,11 +2,11 @@ const decodeTheRing = function (s, p) {
 
     // write your code here
     const dp = Array(s.length + 1).fill(false).map(() => Array(p.length + 1).fill(false));
-    dp[0][0] = true; // Both string and pattern are empty
+    dp[0][0] = true;
 
     for (let j = 1; j <= p.length; j++) {
         if (p[j - 1] === '*') {
-            dp[0][j] = dp[0][j - 1]; // '*' can match empty
+            dp[0][j] = dp[0][j - 1];
         }
     }
 
@@ -15,7 +15,7 @@ const decodeTheRing = function (s, p) {
             if (p[j - 1] === s[i - 1] || p[j - 1] === '?') {
                 dp[i][j] = dp[i - 1][j - 1];
             } else if (p[j - 1] === '*') {
-                dp[i][j] = dp[i - 1][j] || dp[i][j - 1]; // '*' can match any sequence
+                dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
             }
         }
     }
