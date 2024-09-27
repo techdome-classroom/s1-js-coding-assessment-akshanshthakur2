361466ae9f67113dec252d3c93270cedd548ca03
akshanshthakur2 <contact.akshans0@gmail.com>
2024-09-27 12:54:09 +0530

    completed testcase

--- program1.js
+++ program1.js
@@ -3,31 +3,31 @@ const getTotalIsles = function (grid) {
 
   // write your code here
   const rows = grid.length;
-    const cols = grid[0].length;
-    let islandCount = 0;
+  const cols = grid[0].length;
+  let islandCount = 0;
 
-    const dfs = (r, c) => {
-        if (r < 0 || c < 0 || r >= rows || c >= cols || grid[r][c] === 'W') {
-            return;
-        }
-        grid[r][c] = 'W'; // Mark the land as visited
-        // Explore all four directions
-        dfs(r - 1, c); // Up
-        dfs(r + 1, c); // Down
-        dfs(r, c - 1); // Left
-        dfs(r, c + 1); // Right
+  const dfs = (r, c)=>{
+    if (r<0 || c<0 || r>=rows || c>=cols || grid[r][c] === 'W') {
+      return;
+    }
+      grid[r][c] = 'W';
+
+      dfs(r - 1, c);
+      dfs(r + 1, c);
+      dfs(r, c - 1);
+      dfs(r, c + 1);
     };
 
-    for (let r = 0; r < rows; r++) {
-        for (let c = 0; c < cols; c++) {
-            if (grid[r][c] === 'L') {
-                islandCount++;
-                dfs(r, c);
-            }
-        }
+    for (let r=0; r<rows; r++) {
+      for (let c=0; c<cols; c++) {
+          if (grid[r][c] === 'L') {
+              islandCount++;
+              dfs(r, c);
+          }
+      }
     }
 
-    return islandCount;
+  return islandCount;
 
 };
 
--- program2.js
+++ program2.js
@@ -4,14 +4,14 @@ const decodeTheRing = function (s, p) {
     const dp = Array(s.length + 1).fill(false).map(() => Array(p.length + 1).fill(false));
     dp[0][0] = true;
 
-    for (let j = 1; j <= p.length; j++) {
+    for (let j = 1; j<=p.length; j++) {
         if (p[j - 1] === '*') {
             dp[0][j] = dp[0][j - 1];
         }
     }
 
-    for (let i = 1; i <= s.length; i++) {
-        for (let j = 1; j <= p.length; j++) {
+    for (let i = 1; i<=s.length; i++) {
+        for (let j = 1; j<= p.length; j++) {
             if (p[j - 1] === s[i - 1] || p[j - 1] === '?') {
                 dp[i][j] = dp[i - 1][j - 1];
             } else if (p[j - 1] === '*') {
